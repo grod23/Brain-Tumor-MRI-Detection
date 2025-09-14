Patient Separation. Fix Data Leakage

dataset.py
@@ -1,10 +1,15 @@
+# File Handling
 import glob
+import os.path
+import re
+
 import cv2
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
-from PIL import Image
-
+from torchvision.transforms import functional as F
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 # Download latest version
@@ -12,16 +17,45 @@ from PIL import Image
 
 # Normal Image Size is 224x224
 # Image Size Varies
-# 7 images per patient. First image is original while the rest are augmented, so we will only grab the original
+# 7 images per patient. First image is original while the rest are augmented
 # Every 7 images is the original
 def collect_image_paths():
     image_paths = []
     labels = []
 
+    # Gets the File Number in order to sort numerically
+    def extract_number(file_name):
+        match = re.search(r'\d+', file_name)
+        return int(match.group())
+
     def get_images(path, label):
-        for file in (glob.iglob(path)):
-            image_paths.append(file)
-            labels.append(label)
+        # Image Paths for Specific Patient
+        patient_image_paths = []
+        previous_base_name = ' '
+
+        # Sort Files Numerically
+        files = glob.iglob(path)
+        files = sorted(files, key=extract_number)
+
+        # Loop and Categorize Images by Patient
+        for file in files:
+            file_name = os.path.basename(file).removesuffix('.jpg')
+            parts = file_name.split('_')
+            base_name = '_'.join(parts[0:2])
+            # If Base Name Matches Previous Base Name
+            if base_name == previous_base_name:
+                # Add to Patient Array
+                patient_image_paths.append(file)
+            else:
+                # Append Patient Image Paths
+                image_paths.append(patient_image_paths)
+                # Reset Patient Image Paths
+                patient_image_paths.clear()
+                # Append First File Image to New Patient Image Path List
+                patient_image_paths.append(file)
+                # Set New Previous Base Name
+                previous_base_name = base_name
+                labels.append(label)
 
     # Load Image Paths
     get_images("./Brain_Tumor_Dataset/Normal/*.jpg", 0)
@@ -30,27 +64,44 @@ def collect_image_paths():
     get_images("./Brain_Tumor_Dataset/Tumor/pituitary_tumor/*.jpg", 1)
 
     # Return Image Paths and Corresponding Labels
-    return image_paths, torch.tensor(labels, dtype=torch.float32)
+    # print(f'Image Path Length: {len(image_paths)}')
+    # print(f'Lengh of Labels: {len(labels)}')
+    return np.array(image_paths), torch.tensor(labels, dtype=torch.float32)
 
-def z_score_normalization(image):
-    # Image Tensor
-    mean = image.mean()
-    sd = image.std()
-    return (image - mean) / sd
+def compute(image_paths):
+    pixel_values = []
+
+    for path in image_paths:
+        # Read grayscale image as float32 in range [0, 1]
+        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE).astype(np.float32) / 255.0
+        pixel_values.append(img.flatten())
+
+    # Concatenate all pixels from all images
+    all_pixels = np.concatenate(pixel_values)
+
+    mean = np.mean(all_pixels)
+    std = np.std(all_pixels)
+
+    return mean, std
+
+
 
 class MRI(Dataset):
-    def __init__(self, image_paths, labels):
+    def __init__(self, image_paths, labels, testing=False):
         self.image_paths = image_paths
         self.labels = labels
-        # Min-Max Normalization, Resize, and Adjust Tensor Shape
-        self.transform = transforms.Compose([transforms.Resize((224, 224)),
-                                             transforms.ToTensor()
+        # Resize, Adjust Tensor Shape, Min-Max Normalization, Z-Score Normalization
+        self.transform = transforms.Compose([transforms.ToPILImage(),
+                                             transforms.Resize((224, 224)),
+                                             transforms.ToTensor(),
+                                             transforms.Normalize(mean=[0.1891], std=[0.1936])
                                              ])
+        self.testing = testing
 
     def __len__(self):
         return len(self.image_paths)
 
-    def __getitem__(self, index, testing=False):
+    def __getitem__(self, index):
         image_path = self.image_paths[index]
         label = self.labels[index]
 
@@ -60,13 +111,18 @@ class MRI(Dataset):
         # Preprocessing
 
         # Histogram Equalization
-        image = cv2.equalizeHist(image)
-        # Convert to PIL Image
-        image = Image.fromarray(image)
+        # image = cv2.equalizeHist(image)
         # Normalize and Resize
         image = self.transform(image)
-        # Z-Score Normalization
-        image = z_score_normalization(image)
+
+        if self.testing:
+            image = image.numpy()
+
+            # Noise Reduction:
+            # For Salt and Pepper Noise
+            image = cv2.medianBlur(image, 3) # Kernel Size must be odd
+            # Bilateral Filter to smooth preserve edges very well
+            image = cv2.bilateralFilter(image, d=3, sigmaColor=10, sigmaSpace=10)
 
         # For testing:
         # No augmented images
@@ -88,6 +144,7 @@ class MRI(Dataset):
 
     # Standardization(Z-Score Normalization): (X - mean(X)/ SD(X)): - Centers data around 0 with unit variance
     # Improves convergence. Without, optimization may have unwanted oscillations during convergence. Required for MRI
+    # Preferred when outliers are present
 
     # Min-Max Normalization: Scales data point values to be between [0,1]. Typically, not required for MRI, Z-Score
     # Normalization would be more important

main.py
@@ -12,7 +12,7 @@ from model import Model
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader
 from torch.optim import AdamW, lr_scheduler
 
 # Computer Vision Libraries
@@ -22,6 +22,8 @@ from torchvision.utils import make_grid
 
 # Graphing Library
 import matplotlib.pyplot as plt
+import seaborn as sns
+from tqdm import tqdm
 
 # Evaluation Metrics
 from sklearn.metrics import classification_report
@@ -69,7 +71,8 @@ def main():
     batches = 10
     learning_rate = 0.001
     weight_decay = 1e-4
-    loss = nn.BCEWithLogitsLoss()
+    loss_fn = nn.BCEWithLogitsLoss()
+    # Couples with Weight Decay
     optimizer = AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
     # End Training Early if no longer decreasing loss
     scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=5, factor=0.5)
@@ -83,34 +86,107 @@ def main():
     X_temp, X_test, y_temp, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
     X_train, X_val, y_train, y_val = train_test_split(X_temp, y_temp, test_size=0.2, stratify=y_temp, random_state=42)
 
+    # Unravel Data Array, No Longer Need Patient Array now that data is split. No risk of data leakage
+    y_train = y_train.repeat_interleave(X_train.shape[1])
+    y_val = y_val.repeat_interleave(X_val.shape[1])
+    y_test = y_test.repeat_interleave(X_test.shape[1])
+
+    X_train = X_train.reshape(-1)
+    X_val =X_val.reshape(-1)
+    X_test = X_test.reshape(-1)
+
     # Create DataSet Instances
     train_dataset = MRI(X_train, y_train)
     validation_dataset = MRI(X_val, y_val)
-    test_dataset = MRI(X_test, y_test)
+    test_dataset = MRI(X_test, y_test, testing=True)
+
 
     # Create DataLoaders
-
-    # num_workers specify how many parallel subprocesses are used to load the data
-    training_loader = DataLoader(train_dataset, batch_size=32, num_workers=4, shuffle=True)
+    training_loader = DataLoader(train_dataset, batch_size=32, num_workers=4, shuffle=True) # Only Shuffle Training Data
     validation_loader = DataLoader(validation_dataset, batch_size=32, num_workers=4, shuffle=False)
     testing_loader = DataLoader(test_dataset, batch_size=32, num_workers=4, shuffle=False)
+    # Num_workers specify how many parallel subprocesses are used to load the data
+    # DataLoaders also add Batch Size to Shape: (32, 1, 224, 224)
 
+    # Training
+
+    # Loss Tracking
+    loss_track = []
+    # Patience Counter for Early Stopping
+    patience_counter = 0
+
+    # model.train()
+    # for epoch in range(epochs):
+    #     epoch_loss = 0.0
+    #     best_loss = 0.0
+    #     for X_batch, y_batch in training_loader:
+    #         y_predicted = model(X_batch)
+    #         loss = loss_fn(y_predicted, y_batch)
+    #         # Reset Gradients
+    #         optimizer.zero_grad()
+    #         # Backpropagation
+    #         loss.backward()
+    #         # Update Parameters
+    #         optimizer.step()
+    #         # Track Epoch Loss
+    #         epoch_loss += loss.item()
+    #     test_loss = epoch_loss / len(training_loader)
+    #     loss_track.append(test_loss)
+    #     scheduler.step(test_loss)
+    #
+    #     if test_loss < best_loss:
+    #         patience_counter = 0
+    #     else:
+    #         patience_counter += 1
+    #         if patience_counter >= 10:
+    #             print(f'Early Stopping: {}')
 
     # Brain MRI Images Visualization
-    dataset = MRI(X, y)
+    y = y.repeat_interleave(X.shape[1])
+    X = X.reshape(-1)
+    dataset = MRI(X, y, testing=False)
+
+    # replace=False avoids duplicates values
     random_index = np.random.choice(len(X), 6, replace=False)  # Choose 6 Random Indexes for MRI Images
     plt.figure(figsize=(10, 5))
-    print(f'Random Index: {random_index}')
+    # print(f'Random Index: {random_index}')
     # Show Images
     for i in range(6):
         image, label = dataset[random_index[i]]
+        image = image.view(image.shape[2], image.shape[1], image.shape[0])
         plt.subplot(2, 3, i + 1)
-        plt.imshow(image.permute(1, 2, 0))  # Convert CHW -> HWC for matplotlib
+        # Matplotlib takes images as (Height, Width, Channel)
+        plt.imshow(image)  # PyTorch takes images as (Channel, Height, Width)
         plt.title(f"Label: {int(y[random_index[i]])}")
         plt.axis('off')
     plt.tight_layout()
     plt.show()
 
+    # Histogram Plot
+
+    plt.figure(figsize=(10, 9))  # Wider and taller figure
+
+    for i in range(3):
+        image, label = dataset[random_index[i]]
+
+        # Convert from (C, H, W) to (H, W, C) for display
+        image_np = image.permute(1, 2, 0).numpy()
+
+        # Image subplot
+        plt.subplot(3, 2, 2 * i + 1)
+        plt.imshow(image_np)
+        plt.title(f"Image - Label: {int(label)}")
+        plt.axis('off')
+
+        # Histogram subplot
+        plt.subplot(3, 2, 2 * i + 2)
+        sns.histplot(image.numpy().ravel(), kde=True, bins=50, color='skyblue')
+        plt.title("Pixel Intensity Distribution")
+        plt.xlabel("Pixel Value")
+        plt.ylabel("Frequency")
+
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':

model.py
@@ -16,8 +16,8 @@ class Model(nn.Module):
 
         self.conv1 = nn.Conv2d(self.input_features, input1, self.conv_kernel, self.pooling_stride)
         self.conv2 = nn.Conv2d(self.input1, output, self.conv_kernel, self.pooling_stride)
-        self.fc1 = nn.Linear(self.input_features, self.input1)
-        self.fc2 = nn.Linear(self.input1, output)
+        self.fc1 = nn.Linear(10816, 120)
+        self.fc2 = nn.Linear(120, output)
 
     def forward(self, X):
         X = F.relu(self.conv1(X))
@@ -25,7 +25,14 @@ class Model(nn.Module):
         X = F.relu(self.conv2(X))
         X = F.max_pool2d(X, self.pooling_kernel, self.pooling_stride)
         # Flatten
-        # X.view()
+        X = X.view(-1, 10816)
+        # Fully Connected Layers
+        X = F.relu(self.fc1(X))
+        # No Activation Function for last layer
+        X = self.fc2(X)
+        # Apply to convert to probability
+        X = F.log_softmax(X, dim=1)
 
+        return X
 
 