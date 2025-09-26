Data Leakage Fixing

dataset.py
@@ -5,10 +5,14 @@ import re
 
 import cv2
 import torch
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedGroupKFold, GroupShuffleSplit
+from torch.optim.radam import radam
 from torch.utils.data import Dataset
 from torchvision import transforms
 import numpy as np
+import pandas as pd
+import sys
+from collections import defaultdict
 
 
 # Download latest version
@@ -21,6 +25,7 @@ import numpy as np
 def collect_image_paths():
     image_paths = []
     labels = []
+    groups = []
 
     # Gets the File Number in order to sort numerically
     def extract_number(file_name):
@@ -29,8 +34,7 @@ def collect_image_paths():
 
     def get_images(path, label):
         # Image Paths for Specific Patient
-        patient_image_paths = []
-        previous_base_name = ' '
+        patients = defaultdict(list)
 
         # Sort Files Numerically
         files = glob.iglob(path)
@@ -41,22 +45,17 @@ def collect_image_paths():
             file_name = os.path.basename(file).removesuffix('.jpg')
             parts = file_name.split('_')
             base_name = '_'.join(parts[0:2])
-            # If Base Name Matches Previous Base Name
-            if base_name == previous_base_name:
-                # Add to Patient Array
-                patient_image_paths.append(file)
+            patients[base_name].append(file)
 
-            else:
-                # Append Patient Image Paths if it's not empty
-                if patient_image_paths:
-                    image_paths.append(patient_image_paths)
-                    labels.append(label)
-                # Reset Patient Image Paths
-                patient_image_paths.clear()
-                # Append First File Image to New Patient Image Path List
-                patient_image_paths.append(file)
-                # Set New Previous Base Name
-                previous_base_name = base_name
+
+        for patient_id, images in patients.items():
+            if len(images) != 7:
+                raise Exception('Not 7 Images')
+            for image in images:
+                # Append Image Features(image_path, label, and id)
+                image_paths.append(image)
+                labels.append(label)
+                groups.append(patient_id)
 
     # Load Image Paths
     get_images("./Brain_Tumor_Dataset/Normal/*.jpg", 0)
@@ -64,36 +63,42 @@ def collect_image_paths():
     get_images("./Brain_Tumor_Dataset/Tumor/meningioma_tumor/*.jpg", 2)
     get_images("./Brain_Tumor_Dataset/Tumor/pituitary_tumor/*.jpg", 3)
 
-    return np.array(image_paths), torch.LongTensor(labels)
+    return np.array(image_paths), np.array(labels), np.array(groups)
 
-def get_original(patients):
-    # Original Image Paths
-    original = []
+def get_data_split():
+    image_paths, labels, groups = collect_image_paths()
 
-    # Original will be in format 'Letter_Number'
-    for patient in patients:
-        # First image is always the original
-        original_image = patient[0]
-        original.append(original_image)
+    print(f'Total Images: {len(image_paths)}')
+    print(f'Total Labels: {len(labels)}')
+    print(f'Total Groups: {len(np.unique(groups))}')
 
-    return original
+    gss = GroupShuffleSplit(n_splits=1, test_size=0.4, random_state=42)
+    train_index, test_index = next(gss.split(image_paths, labels, groups))
 
-def get_data_split(image_paths, labels):
-    # Split Image Paths into Training, Validation, and Testing
-    # Preserve Class Distribution using stratify
-    X_temp, X_test, y_temp, y_test = train_test_split(image_paths, labels, test_size=0.2, stratify=labels,
-                                                      random_state=42)
-    X_train, X_val, y_train, y_val = train_test_split(X_temp, y_temp, test_size=0.2, stratify=y_temp, random_state=42)
+    X_train = image_paths[train_index]
+    y_train = labels[train_index]
+    X_testing = []
+    y_testing = []
 
-    # Unravel Data Array, No Longer Need Patient Array now that data is split. No risk of data leakage
+    # Remove Augmented Images from Testing
+    for index in test_index:
+        file_name = os.path.basename(image_paths[index]).removesuffix('.jpg').split('_')
+        if len(file_name) == 2:
+            # It is original
+            X_testing.append(image_paths[index])
+            y_testing.append(labels[index])
 
-    # Add Necessary Labels for Unraveled Training Patient Data
-    y_train = y_train.repeat_interleave(X_train.shape[1])
-    # Unravel Training Patient Data
-    X_train = X_train.reshape(-1)
+    print(f'X Train: {len(X_train)}')
+    print(f'y Train: {len(y_train)}')
+    print(f'X Test: {len(X_testing)}')
+    print(f'Y test: {len(y_testing)}')
 
-    # Remove all augmented images from X Test and X Validation
-    X_val, X_test = get_original(X_val), get_original(X_test)
+    X_val, X_test, y_val, y_test = train_test_split(X_testing, y_testing, test_size=0.5, random_state=42, stratify=y_testing)
+
+    # Optional: check for group leakage
+    assert set(groups[train_index]).isdisjoint(set(groups[test_index])), "Group leakage detected!"
+    print(f'Train size: {len(X_train)} images from {len(np.unique(groups[train_index]))} patients')
+    print(f'Test size: {len(X_testing)} images from {len(np.unique(groups[test_index]))} patients')
 
     # Normal: 0
     # Glioma: 1
@@ -101,7 +106,7 @@ def get_data_split(image_paths, labels):
     # Pituitary: 3
 
     return np.array(X_train), torch.LongTensor(y_train), np.array(X_val), torch.LongTensor(y_val), np.array(
-        X_test), torch.LongTensor(y_test),
+        X_testing), torch.LongTensor(y_testing)
 
 def compute(image_paths):
     pixel_values = []
@@ -138,6 +143,10 @@ class MRI(Dataset):
 
     def __getitem__(self, index):
         image_path = self.image_paths[index]
+
+        #if not self.testing:
+            #print(f'Training Image Path: {image_path}')
+
         label = self.labels[index]
 
         # Load Image on Demand
@@ -149,6 +158,7 @@ class MRI(Dataset):
         image = cv2.equalizeHist(image)
 
         if self.testing:
+            #print(f'Validation Image Path: {image_path}')
             # image = image.numpy()
 
             # Noise Reduction: